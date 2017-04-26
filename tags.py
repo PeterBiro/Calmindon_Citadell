2bcd116b07b0309dcc3f0f621770e9f98075ff34
Péter Biró <biro.peter.hu@gmail.com>
2017-04-26 13:51:44 +0200

    get_triple_tags() added

--- tags.py
+++ tags.py
@@ -4,7 +4,7 @@ import collections  # for Counter in count_tags_group_by_name()
 def count_tags_group_by_name(table):
     """
     Counts how many tags are in the table, grouped by their names.
-    Returnds a table in it tags & numbers
+    Returns a table in it tags & numbers
     """
     list_of_tags = []
     for row in table:
@@ -12,6 +12,13 @@ def count_tags_group_by_name(table):
     dict_of_tags = collections.Counter(list_of_tags)
     nr_of_tags = [[key, str(value)] for key, value in dict_of_tags.items()]
     return nr_of_tags
+
+def get_triple_tags(list_of_tags):
+    """
+    Returns the triple tags from a list.
+    e.g. "lvl:8.oszt=könnyű"
+    """
+    return list(filter(lambda x: x.startswith("lvl:"), list_of_tags))
 
 
 def filter_table(table, filter_tags):
